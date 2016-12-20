80437a63823712b2b387a8b8b631bcf19e1728fa
Bruce Liu <changliu818@yahoo.com>
2016-12-20 15:32:29 -0800

    [prog] Enable basic single player game

--- server/config/socket.config.js
+++ server/config/socket.config.js
@@ -1,14 +1,30 @@
+const shortid = require('shortid');
+
+let openConnections = {};
 
 module.exports = (server) => {
   const io = require('socket.io')(server);
 
   io.on('connection', (socket) => {
+    openConnections[socket.id] = {socket: socket};
+    let connection = openConnections[socket.id];
     console.log('Server socket is up');
     socket.emit('user connected');
 
-    socket.on('message', (data) => {
-      console.log('message from client', data);
-      socket.emit('message', data);
+    socket.on('message', data => {
+      connection.users = [];
+    });
+
+    // incoming data should include the "user" who is requesting to create this room
+    socket.on('newSinglePlayerRoom', (data) => {
+      connection.users = [].push(data.user);          // add a user to the room
+      let roomId = shortid.generate();
+      connection.roomId = roomId;                     // attach roomId to room
+      socket.join('room-' + roomId);
+      // Add the new room to DB and (redis?)
+      io.sockets.in('room-' + roomId).emit('roomCreated', {     // only emit to sockets in that room
+        roomId: roomId
+      });
     });
   });
 };
