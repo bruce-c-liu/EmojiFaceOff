2a4961cb1c8b41dc643efeb0bf0817844ea0bf22
Bruce Liu <changliu818@yahoo.com>
2016-12-21 09:47:24 -0800

    [prog] Socket rooms

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -3,65 +3,65 @@ import io from 'socket.io-client';
 import Bubble from './Bubble';
 // import iphone from '../../assets/iphone.png';
 // console.log("IPHONE",iphone )
-	
+
 const socket = io('http://localhost:3000');
 
-class Chat  extends Component{
-constructor(){
-	super()
-	this.state = {
-		message: '',
-		user: '',
-		chats: []
-	}
-	socket.on('message', (message) => {
-		this.setState({chats: [...this.state.chats, message] })									
-	})
+class Chat extends Component {
+  constructor () {
+  super();
+  this.state = {
+  message: '',
+  user: '',
+  chats: []
+};
+  socket.on('message', (message) => {
+  this.setState({chats: [...this.state.chats, message] });
+});
 }
-componentDidMount(){
+  componentDidMount () {
 
 }
-handleNameChange(e){
-	this.setState({
-		user: e.target.value
-	})
-}
-handleChange(e){
-	this.setState({
-		message: e.target.value
-	})
-}
-sendMessage(e){
-	e.preventDefault();
-	const userMessage = { user: this.state.user, text: this.state.message }
-	socket.emit('message',  userMessage)
-	this.setState({
-		chats: [...this.state.chats, userMessage],
-		message: '',
-		user: ''
-	})
-}	
+  handleNameChange (e) {
+    this.setState({
+      user: e.target.value
+    });
+  }
+  handleChange (e) {
+    this.setState({
+      message: e.target.value
+    });
+  }
+  sendMessage (e) {
+    e.preventDefault();
+    const userMessage = { user: this.state.user, text: this.state.message };
+    socket.emit('message', userMessage);
+    socket.emit('newSinglePlayerRoom');
+    this.setState({
+      chats: [...this.state.chats, userMessage],
+      message: '',
+      user: ''
+    });
+  }
   render () {
-  	const chatList = this.state.chats.map( (item, i)=>{
-  		return <Bubble deets={item} key={i}/>
-  	})
-  	const roomID = this.props.params.roomID
-  	console.log(roomID )
-  		
+  	const chatList = this.state.chats.map((item, i) => {
+  		return <Bubble deets={item} key={i} />;
+  	});
+  	const roomID = this.props.params.roomID;
+  	console.log(roomID);
 
     return (
-      <div className="chat-view">
-      		<h3>ROOM ID / SOCKET ROOM ID:  { roomID }</h3>
-      		<div className="chat-messages">
-      			{chatList}
+      <div className='chat-view'>
+      		      <h3>ROOM ID / SOCKET ROOM ID:     { roomID }</h3>
+      		      <div className='chat-messages'>
+      			      {chatList}
       		</div>
-      		<form onSubmit={this.sendMessage.bind(this)}>
-      			<input type="text" value={this.state.user} onChange={this.handleNameChange.bind(this)} placeholder="Name"/>
-      			<input type="text" value={this.state.message} onChange={this.handleChange.bind(this)} placeholder="Message"/>
-      			<input type="submit" value="Submit"/>
+      		      <form onSubmit={this.sendMessage.bind(this)}>
+      			      <input type='text' value={this.state.user} onChange={this.handleNameChange.bind(this)} placeholder='Name' />
+      			      <input type='text' value={this.state.message} onChange={this.handleChange.bind(this)} placeholder='Message' />
+      			      <input type='submit' value='Submit' />
       		</form>
       </div>
-    )
+    );
   }
 }
-export default Chat
+export default Chat;
--- server/config/socket.config.js
+++ server/config/socket.config.js
@@ -1,5 +1,5 @@
 const shortid = require('shortid');
-
+const RedisController = require('../db/Redis/RedisController.js');
 let openConnections = {};
 
 module.exports = (server) => {
@@ -8,23 +8,117 @@ module.exports = (server) => {
   io.on('connection', (socket) => {
     openConnections[socket.id] = {socket: socket};
     let connection = openConnections[socket.id];
-    console.log('Server socket is up');
+    RedisController.getPrompts(1).then(prompts => console.log(prompts));
     socket.emit('user connected');
 
+    let roundNum;                  // via redis: current round of this room
+    let promptIndex;               // calculated here when necessary
+    let prompt;                    // via redis: current prompt that is being worked on
+    let botResponse;               // created on message
+    let userMessage;               // alias for incoming user's text
+    let usersInRoom;               // via redis: retrieve all users in this room.
+    let level;                     // via redis: difficulty of this room right now
+
     socket.on('message', data => {
-      connection.users = [];
+      let userMessage = data.text;
+      let level = data.level;
+      let roundNum = data.roundNum;
+      let prompt = data.prompt;
+      RedisController.getPrompts(level)
+        .then(prompts => {
+          let libraryFilteredByLevel = prompts;
+        });
+
+      if (roundNum === 0) {
+        if (userMessage === 'start') {
+          RedisController.getPrompts(level)
+        .then(prompts => {
+          let libraryFilteredByLevel = prompts;
+        });
+          do {                              // Find a new selectable prompt at random.
+            promptIndex = Math.floor(Math.random() * libraryFilteredByLevel.length);
+          } while (libraryFilteredByLevel[promptIndex].selectable === false);
+          prompt = libraryFilteredByLevel[promptIndex];
+          botResponse = {
+            'user': 'ebot',
+            'text': `Welcome to Emoji Face Off! 
+                 Round ${roundNum}
+                 Please translate '${prompt.prompt}' into emoji form~`
+          };
+          socket.emit('message', botResponse);
+        } else {
+          botResponse = {
+            'user': 'ebot',
+            'text': `Send 'start' to begin the game, dumbass.`
+          };
+          socket.emit('message', botResponse);
+        }
+      } else if (roundNum <= 5) {
+        if (prompt.answers.includes(userMessage)) {        // A user replied with a correct answer.
+          prompt.selectable = false;                       // Make the prompt not selectable anymore.
+          for (let user of usersInRoom) {                  // Increment the user's score.
+            if (user.name === data.user) {
+              user.score++;
+            }
+          }
+
+          if (roundNum < 5) {
+            roundNum++;                                  // Increment round number.
+            do {                                         // Find a new selectable prompt at random.
+              promptIndex = Math.floor(Math.random() * libraryFilteredByLevel.length);
+            } while (libraryFilteredByLevel[promptIndex].selectable === false);
+            prompt = libraryFilteredByLevel[promptIndex];
+            botResponse = {
+              'user': 'ebot',
+              'text': `Good job, ${data.user}! 
+                   Round ${roundNum}
+                   Please translate '${prompt.prompt}' into emoji form~`
+            };
+            socket.emit('message', botResponse);
+          } else if (roundNum === 5) {                   // Current game has ended.
+            roundNum = 0;                                // Reset round number.
+            let winner = usersInRoom.reduce((prevUser, currUser) => {
+              if (currUser.score > prevUser.score) {
+                return currUser;
+              }
+            })[0];
+            botResponse = {
+              'user': 'ebot',
+              'text': `Good job, ${data.user}!
+                   The winner is ${winner.name} with ${winner.score} points!
+                   Send 'start' to begin a new game.`
+            };
+            usersInRoom.forEach(user => {              // Reset user scores.
+              user.score = 0;
+            });
+          }
+        } else {                                       // A user replied with an incorrect answer.
+          botResponse = {
+            'user': 'ebot',
+            'text': `That is not the correct answer, ${data.user}!`
+          };
+          socket.emit('message', botResponse);
+        }
+      }
     });
 
     // incoming data should include the "user" who is requesting to create this room
     socket.on('newSinglePlayerRoom', (data) => {
-      connection.users = [].push(data.user);          // add a user to the room
       let roomId = shortid.generate();
-      connection.roomId = roomId;                     // attach roomId to room
-      socket.join('room-' + roomId);
+      socket.join(roomId);
       // Add the new room to DB and (redis?)
-      io.sockets.in('room-' + roomId).emit('roomCreated', {     // only emit to sockets in that room
+      io.sockets.in(roomId).emit('roomCreated', {     // only emit to sockets in that room
         roomId: roomId
       });
+      let room = io.nsps['/'].adapter.rooms[roomId];
+      room.level = 1;
+      room.round = 0;
+      room.prompt = '';
+      room.host = '';
+      connection.roomId = roomId;
+      // console.log(room);
+      // console.log(socket.nsp.adapter.rooms);
+      // console.log('LOOK!', openConnections);
     });
   });
 };
