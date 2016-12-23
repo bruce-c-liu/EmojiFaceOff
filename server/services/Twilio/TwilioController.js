9ec8aedc0ce07918e0fa83cd059d9a962ae2cac3
Khoa Nguyen <knb.nguyen8@gmail.com>
2016-12-22 16:53:38 -0800

    [feature] able to send invites via text

---
+++ server/services/Twilio/TwilioController.js
@@ -0,0 +1,35 @@
+
+const twilClient = require('../../config/twilio.config.js');
+let Promise = require('bluebird');
+
+module.exports = {
+
+  textMessage: (req, res, next) => {
+    console.log('Trying to invite ', req.body);
+    /**
+     * Expects:
+     *  numbers: array,
+     *  userName: string,
+     *  roomUrl: string
+     */
+    let message = `${req.body.userName} has invited you to play EmojiFaceOff! Click to join game, ${req.body.roomUrl} `;
+    let numbers = req.body.numbers.map(num => {
+      return twilClient.sendMessage({
+        to: num,
+        from: process.env.TWILIO_CALLER_ID,
+        body: message
+      });
+    });
+
+    Promise.all(numbers)
+    .then(result => {
+      console.log('success sending game invites via texts!');
+      res.json(result);
+    })
+    .catch(err => {
+      res.send(404);
+      throw err;
+    });
+  }
+
+};
