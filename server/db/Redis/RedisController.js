b8b8eb2d20725a9fa0b81931e96ebfa3efc10c05
Bruce Liu <changliu818@yahoo.com>
2016-12-28 22:23:53 -0800

    [refactor+feature] Reduce redis calls to minimum. Any skin tone is now acceptable answer.

--- server/db/Redis/RedisController.js
+++ server/db/Redis/RedisController.js
@@ -12,13 +12,15 @@ module.exports = {
     else return redClient.sunion(`L0P`, `L1P`, `L2P`, `L3P`, `L4P`, `L5P`);
   },
 
+  // Decprecated
   checkAnswer: (prompt, answer) => {
     let tmp = answer;
-    console.log('\u{1F4AA}' === '💪');
+    // console.log('\u{1F4AA}' === '💪');
     console.log('checking answer....', prompt, tmp);
     return redClient.smembers(`PTA:${prompt}`)
       .then(answers => {
         console.log('ANSWERS:', answers);
+
         for (let a of answers) {
           console.log(a, tmp, a === tmp);
           if (a === tmp) {
@@ -30,6 +32,14 @@ module.exports = {
       .catch(err => {
         throw err;
       });
+  },
+
+  getAnswers: (prompt) => {
+    return redClient.smembers(`PTA:${prompt}`)
+      .catch(err => {
+        throw err;
+      });
   }
 
 };
+
